b7e418fd1cd210bfdab01a06d0cd1a9b72440921
CF <422081094@qq.com>
2020-04-19 22:04:14 +0800

    add: 根据父组件传的 type 生成不同的 button

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="root">
-    <div id="app">{{msg}}</div>
-    <my-button>TEST</my-button>
+    <div class="btn-content">
+      <my-button v-for="t in types" class="btn" :type="t" :key="t">TEST</my-button>
+    </div>
   </div>
 </template>
 
@@ -10,8 +11,16 @@ export default {
   name: "app",
   data() {
     return {
-      msg: "Hello Vue"
+      msg: "Hello Vue",
+      types: ["primary", "success", "info", "warning", "danger"]
     };
   }
 };
 </script>
+<style lang="less">
+.btn-content {
+  .btn {
+    margin-left: 10px;
+  }
+}
+</style>
--- src/components/button.vue
+++ src/components/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="my-button">
+  <button class="my-button" :class="[`my-button--${type}`]">
     <span>
       <slot></slot>
     </span>
@@ -7,8 +7,15 @@
 </template>
 <script>
 export default {
-  name: 'MyButton'
-}
+  name: "MyButton",
+  props: {
+    type: {
+      // 数据类型：字符串
+      type: String,
+      default: 'primary'
+    }
+  }
+};
 </script>
 
 <style lang="less">
@@ -32,10 +39,70 @@ export default {
   font-size: 14px;
   border-radius: 4px;
   &:hover,
-  &:hover{
+  &:focus {
     color: #409eff;
     border-color: #c6e2ff;
     background-color: #ecf5ff;
+  }
+}
+.my-button--primary {
+  color: #fff;
+  background-color: #409eff;
+  border-color: #409eff;
+
+  &:hover,
+  &:focus {
+    color: #fff;
+    background-color: #66b1ff;
+    border-color: #66b1ff;
+  }
+}
+.my-button--success {
+  color: #fff;
+  background-color: #67c23a;
+  border-color: #67c23a;
+
+  &:hover,
+  &:focus {
+    color: #fff;
+    background-color: #85ce61;
+    border-color: #85ce61;
+  }
+}
+.my-button--info {
+  color: #fff;
+  background-color: #909399;
+  border-color: #909399;
+
+  &:hover,
+  &:focus {
+    color: #fff;
+    background-color: #a6a9ad;
+    border-color: #a6a9ad;
+  }
+}
+.my-button--warning {
+  color: #fff;
+  background-color: #e6a23c;
+  border-color: #e6a23c;
+
+  &:hover,
+  &:focus {
+    color: #fff;
+    background-color: #ebb563;
+    border-color: #ebb563;
+  }
+}
+.my-button--danger {
+  color: #fff;
+  background-color: #f56c6c;
+  border-color: #f56c6c;
+
+  &:hover,
+  &:focus {
+    color: #fff;
+    background-color: #f78989;
+    border-color: #f78989;
   }
 }
 </style>
